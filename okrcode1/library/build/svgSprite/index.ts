f70bfbbe9495aa5efd655da26298e82530d6f0ee
Davie Jose <1239045617@qq.com>
2025-04-22 11:05:09 +0800

    Delete okrcode1 directory

--- okrcode1/library/build/svgSprite/index.ts
+++
@@ -1,9 +0,0 @@
-import path from 'path'
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
-
-export const createSvgIcons = () => {
-  return createSvgIconsPlugin({
-    iconDirs: [path.resolve(process.cwd(), 'src/icon')],
-    symbolId: 'vab-icon-[name]',
-  })
-}
